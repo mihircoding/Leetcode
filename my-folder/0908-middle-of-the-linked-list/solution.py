d1b0d8b688c61245934e44994f85bb20feff5188
Mihir Konda <84816566+mihircoding@users.noreply.github.com>
2022-08-29 04:02:26 +0000

    [LeetCode Sync] Runtime - 53 ms (5.16%), Memory - 13.8 MB (100.00%)

---
+++ my-folder/0908-middle-of-the-linked-list/solution.py
@@ -0,0 +1,8 @@
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        a = []
+        while head:
+            a.append(head)
+            head = head.next
+        return a[len(a)//2]
