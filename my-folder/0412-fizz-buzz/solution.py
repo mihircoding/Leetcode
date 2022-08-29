d95f9917776372b571ae1bb8b1ea5261bd99b0af
Mihir Konda <84816566+mihircoding@users.noreply.github.com>
2022-08-29 03:42:01 +0000

    [LeetCode Sync] Runtime - 67 ms (5.88%), Memory - 15.1 MB (100.00%)

---
+++ my-folder/0412-fizz-buzz/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ansList = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                ansList.append("FizzBuzz")
+            elif i %3 == 0:
+                ansList.append("Fizz")
+            elif i %5 == 0:
+                ansList.append("Buzz")
+            else:
+                ansList.append(str(i))
+        return ansList
