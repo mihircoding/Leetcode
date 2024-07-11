043292a2ca9235eefd09cbf308fb7f838d4bb553
Mihir Konda <84816566+mihircoding@users.noreply.github.com>
2024-07-11 12:23:36 +0000

    [LeetCode Sync] Runtime - 36 ms (84.00%), Memory - 17 MB (17.43%)

---
+++ my-folder/0274-h-index/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        for i in range(len(citations)):
+            if len(citations) - i <= citations[i]:
+                return len(citations) - i
+
+        return 0
